17a70d8f1889b81772907a8fc49bfafff03caadc
Gaute <mrboobai@proton.me>
2023-04-18 13:58:34 +0200

    spaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 
 const char* ssid     = "theoffice";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "tronderbest";     // The password of the Wi-Fi network
+
+int trigPin = 12;    // TRIG pin
+int echoPin = 14;    // ECHO pin
+float duration_us, distance_cm;
+
+const int PIN_RED   = 16;
+const int PIN_GREEN = 5;
+const int PIN_BLUE  = 4;
+
+void setColor(int R, int G, int B) {
+  analogWrite(PIN_RED,   R);
+  analogWrite(PIN_GREEN, G);
+  analogWrite(PIN_BLUE,  B);
+}
 
 void setup() {
   Serial.begin(9600);         // Start the Serial communication to send messages to the computer
@@ -24,27 +37,7 @@ void setup() {
   Serial.println("Connection established!");  
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-}
 
-void loop() { }
-
-
-int trigPin = 12;    // TRIG pin
-int echoPin = 14;    // ECHO pin
-float duration_us, distance_cm;
-
-const int PIN_RED   = 16;
-const int PIN_GREEN = 5;
-const int PIN_BLUE  = 4;
-
-void setColor(int R, int G, int B) {
-  analogWrite(PIN_RED,   R);
-  analogWrite(PIN_GREEN, G);
-  analogWrite(PIN_BLUE,  B);
-}
-
-
-void setup() {
   // begin serial port
   Serial.begin (9600);
   pinMode(PIN_RED,   OUTPUT);
@@ -73,14 +66,15 @@ void loop() {
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
-  if (distance_cm < 120) {
-    Serial.print("Fult plass");
-        setColor(0, 255, 255);
-
-  } else {
-        Serial.print("Det er plass");
-            setColor(0, 0, 255);
-
-    }
+  if (distance_cm < 120) 
+  {
+  Serial.print("Fult plass");
+  setColor(0, 255, 255);
+  } 
+  else 
+  {
+  Serial.print("Det er plass");
+  setColor(0, 0, 255);
+  }
   delay(500);
 }
