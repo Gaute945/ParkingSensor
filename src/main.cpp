da822da0e98009236dc8dd944f5792d0a02dae8b
Gaute <mrboobai@proton.me>
2023-04-18 10:46:31 +0200

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  int trigPin = 12;    // TRIG pin
+  int echoPin = 14;    // ECHO pin
+
+  float duration_us, distance_cm;
+  // begin serial port
+  Serial.begin (9600);
+
+  // configure the trigger pin to output mode
+  pinMode(trigPin, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
