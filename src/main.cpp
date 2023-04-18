f2cfa6d3a44878a15e69eef05a26ff38f21b2bfb
11int <miladd2006@gmail.com>
2023-04-18 13:02:17 +0200

    Hey work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
+
 #include <Arduino.h>
 
 int trigPin = 12;    // TRIG pin
 int echoPin = 14;    // ECHO pin
 float duration_us, distance_cm;
 
+const int PIN_RED   = 16;
+const int PIN_GREEN = 5;
+const int PIN_BLUE  = 4;
+
+void setColor(int R, int G, int B) {
+  analogWrite(PIN_RED,   R);
+  analogWrite(PIN_GREEN, G);
+  analogWrite(PIN_BLUE,  B);
+}
+
+
 void setup() {
   // begin serial port
   Serial.begin (9600);
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
 
   // configure the trigger pin to output mode
   pinMode(trigPin, OUTPUT);
@@ -30,6 +45,15 @@ void loop() {
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
+  if (distance_cm < 120) {
+    Serial.print("Fult plass");
+        setColor(0, 255, 255);
+
+  } else {
+        Serial.print("Det er plass");
+            setColor(0, 0, 255);
+
+    }
 
   delay(500);
 }
