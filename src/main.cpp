499a192f6500f4e8e3fd135d40a5febb4e98fe78
11int <miladd2006@gmail.com>
2023-04-18 13:55:29 +0200

    wifi action

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 
 #include <Arduino.h>
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+
+const char* ssid     = "theoffice";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "tronderbest";     // The password of the Wi-Fi network
+
+void setup() {
+  Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+}
+
+void loop() { }
+
 
 int trigPin = 12;    // TRIG pin
 int echoPin = 14;    // ECHO pin
@@ -54,6 +82,5 @@ void loop() {
             setColor(0, 0, 255);
 
     }
-
   delay(500);
 }
