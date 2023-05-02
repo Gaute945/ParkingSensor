a7be98a334b8d2f393ce20d1be6a598e1977408d
Gaute <mrboobai@proton.me>
2023-05-02 09:52:24 +0200

    spaces

--- src/main.cpp
+++ src/main.cpp
@@ -4,47 +4,52 @@
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "theoffice";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "tronderbest";     // The password of the Wi-Fi network
+const char *ssid = "theoffice";       // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "tronderbest"; // The password of the Wi-Fi network
 HTTPClient http;
 WiFiClient wifiClient;
-int trigPin = 12;    // TRIG pin
-int echoPin = 14;    // ECHO pin
+int trigPin = 12; // TRIG pin
+int echoPin = 14; // ECHO pin
 float duration_us, distance_cm;
 
-const int PIN_RED   = 16;
+const int PIN_RED = 16;
 const int PIN_GREEN = 5;
-const int PIN_BLUE  = 4;
+const int PIN_BLUE = 4;
 
-void setColor(int R, int G, int B) {
-  analogWrite(PIN_RED,   R);
+void setColor(int R, int G, int B)
+{
+  analogWrite(PIN_RED, R);
   analogWrite(PIN_GREEN, G);
-  analogWrite(PIN_BLUE,  B);
+  analogWrite(PIN_BLUE, B);
 }
 
-void setup() {
-  Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+void setup()
+{
+  Serial.begin(9600); // Start the Serial communication to send messages to the computer
   delay(10);
   Serial.println('\n');
-  
-  WiFi.begin(ssid, password);             // Connect to the network
+
+  WiFi.begin(ssid, password); // Connect to the network
   Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
+  Serial.print(ssid);
+  Serial.println(" ...");
 
   int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    Serial.print(++i);
+    Serial.print(' ');
   }
 
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
   Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
   // begin serial port
-  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
   pinMode(PIN_GREEN, OUTPUT);
-  pinMode(PIN_BLUE,  OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
 
   // configure the trigger pin to output mode
   pinMode(trigPin, OUTPUT);
@@ -52,7 +57,8 @@ void setup() {
   pinMode(echoPin, INPUT);
 }
 
-float getSensorData() {
+float getSensorData()
+{
   // generate 10-microsecond pulse to TRIG pin
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -65,27 +71,27 @@ float getSensorData() {
   return 0.017 * duration_us;
 }
 
-void loop() {
+void loop()
+{
   const float distance_cm = getSensorData();
- 
-
 
   // print the value to Serial Monitor
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
 
-  if (distance_cm < 220) 
+  if (distance_cm < 220)
   {
     Serial.println("Fult plass");
     setColor(0, 255, 255);
-  } 
-  else 
+  }
+  else
   {
     Serial.println("Det er plass");
     setColor(0, 0, 255);
   }
-   if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     http.begin(wifiClient, "http://192.168.2.233:8000/sensor");
     http.addHeader("Content-Type", "application/json");
 
